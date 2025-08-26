9c36c75a799a3f907b9369dcf61a9a8e9ca2c4b9
justin cale <justin@innertickets.com>
2025-08-26 14:05:35 +0800

    Updated to use Flow.Core 4.0.0

--- Client/Modules/Dev1.Module.GoogleAdmin.Calendar/ModuleInfo.cs
+++ Client/Modules/Dev1.Module.GoogleAdmin.Calendar/ModuleInfo.cs
@@ -12,7 +12,7 @@ namespace Dev1.Module.GoogleAdmin.Calendar
             Version = "1.0.0",
             ServerManagerType = "Dev1.Module.GoogleAdmin.Manager.GoogleAdminManager, Dev1.Module.GoogleAdmin.Server.Oqtane",
             ReleaseVersions = "1.0.0",
-            Dependencies = "Dev1.Module.GoogleAdmin.Shared.Oqtane,Dev1.Flow.Core,Radzen.Blazor,NodaTime,"+
+            Dependencies = "Dev1.Module.GoogleAdmin.Shared.Oqtane,Radzen.Blazor,NodaTime,"+
             "Google.Apis.Admin.Directory.directory_v1," +
             "Google.Apis.Auth," +
             "Google.Apis.Calendar.v3," +
--- Package/debug.cmd
+++ Package/debug.cmd
@@ -1,34 +1,34 @@
-XCOPY "..\Client\bin\Debug\net9.0\Dev1.Module.GoogleAdmin.Client.Oqtane.dll" "..\..\oqtane.framework613\Oqtane.Server\bin\Debug\net9.0\" /Y
-XCOPY "..\Client\bin\Debug\net9.0\Dev1.Module.GoogleAdmin.Client.Oqtane.pdb" "..\..\oqtane.framework613\Oqtane.Server\bin\Debug\net9.0\" /Y
-XCOPY "..\Server\bin\Debug\net9.0\Dev1.Module.GoogleAdmin.Server.Oqtane.dll" "..\..\oqtane.framework613\Oqtane.Server\bin\Debug\net9.0\" /Y
-XCOPY "..\Server\bin\Debug\net9.0\Dev1.Module.GoogleAdmin.Server.Oqtane.pdb" "..\..\oqtane.framework613\Oqtane.Server\bin\Debug\net9.0\" /Y
-XCOPY "..\Shared\bin\Debug\net9.0\Dev1.Module.GoogleAdmin.Shared.Oqtane.dll" "..\..\oqtane.framework613\Oqtane.Server\bin\Debug\net9.0\" /Y
-XCOPY "..\Shared\bin\Debug\net9.0\Dev1.Module.GoogleAdmin.Shared.Oqtane.pdb" "..\..\oqtane.framework613\Oqtane.Server\bin\Debug\net9.0\" /Y
+XCOPY "..\Client\bin\Debug\net9.0\Dev1.Module.GoogleAdmin.Client.Oqtane.dll" "..\..\oqtane.framework\Oqtane.Server\bin\Debug\net9.0\" /Y
+XCOPY "..\Client\bin\Debug\net9.0\Dev1.Module.GoogleAdmin.Client.Oqtane.pdb" "..\..\oqtane.framework\Oqtane.Server\bin\Debug\net9.0\" /Y
+XCOPY "..\Server\bin\Debug\net9.0\Dev1.Module.GoogleAdmin.Server.Oqtane.dll" "..\..\oqtane.framework\Oqtane.Server\bin\Debug\net9.0\" /Y
+XCOPY "..\Server\bin\Debug\net9.0\Dev1.Module.GoogleAdmin.Server.Oqtane.pdb" "..\..\oqtane.framework\Oqtane.Server\bin\Debug\net9.0\" /Y
+XCOPY "..\Shared\bin\Debug\net9.0\Dev1.Module.GoogleAdmin.Shared.Oqtane.dll" "..\..\oqtane.framework\Oqtane.Server\bin\Debug\net9.0\" /Y
+XCOPY "..\Shared\bin\Debug\net9.0\Dev1.Module.GoogleAdmin.Shared.Oqtane.pdb" "..\..\oqtane.framework\Oqtane.Server\bin\Debug\net9.0\" /Y
 
 
-XCOPY "..\Server\bin\Debug\net9.0\Dev1.Module.Flow.Core.dll" "..\..\oqtane.framework613\Oqtane.Server\bin\Debug\net9.0\" /Y
-XCOPY "..\Server\bin\Debug\net9.0\Dev1.Module.Flow.Core.pdb" "..\..\oqtane.framework613\Oqtane.Server\bin\Debug\net9.0\" /Y
+XCOPY "..\Server\bin\Debug\net9.0\Dev1.Module.Flow.Core.dll" "..\..\oqtane.framework\Oqtane.Server\bin\Debug\net9.0\" /Y
+XCOPY "..\Server\bin\Debug\net9.0\Dev1.Module.Flow.Core.pdb" "..\..\oqtane.framework\Oqtane.Server\bin\Debug\net9.0\" /Y
 
-XCOPY "..\Server\bin\Debug\net9.0\Google.Apis.dll" "..\..\oqtane.framework613\Oqtane.Server\bin\Debug\net9.0\" /Y
-XCOPY "..\Server\bin\Debug\net9.0\Google.Apis.pdb" "..\..\oqtane.framework613\Oqtane.Server\bin\Debug\net9.0\" /Y
-XCOPY "..\Server\bin\Debug\net9.0\Google.Apis.Core.dll" "..\..\oqtane.framework613\Oqtane.Server\bin\Debug\net9.0\" /Y
-XCOPY "..\Server\bin\Debug\net9.0\Google.Apis.Core.pdb" "..\..\oqtane.framework613\Oqtane.Server\bin\Debug\net9.0\" /Y
+XCOPY "..\Server\bin\Debug\net9.0\Google.Apis.dll" "..\..\oqtane.framework\Oqtane.Server\bin\Debug\net9.0\" /Y
+XCOPY "..\Server\bin\Debug\net9.0\Google.Apis.pdb" "..\..\oqtane.framework\Oqtane.Server\bin\Debug\net9.0\" /Y
+XCOPY "..\Server\bin\Debug\net9.0\Google.Apis.Core.dll" "..\..\oqtane.framework\Oqtane.Server\bin\Debug\net9.0\" /Y
+XCOPY "..\Server\bin\Debug\net9.0\Google.Apis.Core.pdb" "..\..\oqtane.framework\Oqtane.Server\bin\Debug\net9.0\" /Y
 
-XCOPY "..\Server\bin\Debug\net9.0\Google.Apis.Auth.dll" "..\..\oqtane.framework613\Oqtane.Server\bin\Debug\net9.0\" /Y
-XCOPY "..\Server\bin\Debug\net9.0\Google.Apis.Auth.pdb" "..\..\oqtane.framework613\Oqtane.Server\bin\Debug\net9.0\" /Y
+XCOPY "..\Server\bin\Debug\net9.0\Google.Apis.Auth.dll" "..\..\oqtane.framework\Oqtane.Server\bin\Debug\net9.0\" /Y
+XCOPY "..\Server\bin\Debug\net9.0\Google.Apis.Auth.pdb" "..\..\oqtane.framework\Oqtane.Server\bin\Debug\net9.0\" /Y
 
-XCOPY "..\Server\bin\Debug\net9.0\Google.Apis.Calendar.v3.dll" "..\..\oqtane.framework613\Oqtane.Server\bin\Debug\net9.0\" /Y
-XCOPY "..\Server\bin\Debug\net9.0\Google.Apis.Calendar.v3.pdb" "..\..\oqtane.framework613\Oqtane.Server\bin\Debug\net9.0\" /Y
+XCOPY "..\Server\bin\Debug\net9.0\Google.Apis.Calendar.v3.dll" "..\..\oqtane.framework\Oqtane.Server\bin\Debug\net9.0\" /Y
+XCOPY "..\Server\bin\Debug\net9.0\Google.Apis.Calendar.v3.pdb" "..\..\oqtane.framework\Oqtane.Server\bin\Debug\net9.0\" /Y
 
 XCOPY "..\Server\bin\Debug\net9.0\Google.Apis.Admin.Directory.directory_v1.dll" "..\..\oqtane.framework613\Oqtane.Server\bin\Debug\net9.0\" /Y
 XCOPY "..\Server\bin\Debug\net9.0\Google.Apis.Admin.Directory.directory_v1.pdb" "..\..\oqtane.framework613\Oqtane.Server\bin\Debug\net9.0\" /Y
 
-XCOPY "..\Server\bin\Debug\net9.0\Google.Apis.Drive.V3.dll" "..\..\oqtane.framework613\Oqtane.Server\bin\Debug\net9.0\" /Y
-XCOPY "..\Server\bin\Debug\net9.0\Google.Apis.Drive.V3.pdb" "..\..\oqtane.framework613\Oqtane.Server\bin\Debug\net9.0\" /Y
+XCOPY "..\Server\bin\Debug\net9.0\Google.Apis.Drive.V3.dll" "..\..\oqtane.framework\Oqtane.Server\bin\Debug\net9.0\" /Y
+XCOPY "..\Server\bin\Debug\net9.0\Google.Apis.Drive.V3.pdb" "..\..\oqtane.framework\Oqtane.Server\bin\Debug\net9.0\" /Y
 
-XCOPY "..\Server\bin\Debug\net9.0\Radzen.Blazor.dll" "..\..\oqtane.framework613\Oqtane.Server\bin\Debug\net9.0\" /Y
+XCOPY "..\Server\bin\Debug\net9.0\Radzen.Blazor.dll" "..\..\oqtane.framework\Oqtane.Server\bin\Debug\net9.0\" /Y
 
-XCOPY "..\Server\bin\Debug\net9.0\NodaTime.dll" "..\..\oqtane.framework613\Oqtane.Server\bin\Debug\net9.0\" /Y
-XCOPY "..\Server\bin\Debug\net9.0\Dev1.QandA.Core.dll" "..\..\oqtane.framework613\Oqtane.Server\bin\Debug\net9.0\" /Y
-XCOPY "..\Server\bin\Debug\net9.0\GoogleApi.dll" "..\..\oqtane.framework613\Oqtane.Server\bin\Debug\net9.0\" /Y
-XCOPY "..\Server\wwwroot\*" "..\..\oqtane.framework613\Oqtane.Server\wwwroot\" /Y /S /I
+XCOPY "..\Server\bin\Debug\net9.0\NodaTime.dll" "..\..\oqtane.framework\Oqtane.Server\bin\Debug\net9.0\" /Y
+XCOPY "..\Server\bin\Debug\net9.0\Dev1.QandA.Core.dll" "..\..\oqtane.framework\Oqtane.Server\bin\Debug\net9.0\" /Y
+XCOPY "..\Server\bin\Debug\net9.0\GoogleApi.dll" "..\..\oqtane.framework\Oqtane.Server\bin\Debug\net9.0\" /Y
+XCOPY "..\Server\wwwroot\*" "..\..\oqtane.framework\Oqtane.Server\wwwroot\" /Y /S /I
